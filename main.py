f22051e84a8eec04d1ff24a9d03a696badaac1a7
BK CHAITANYA <35691586+BKCHAITANYA@users.noreply.github.com>
2023-03-09 20:28:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import fastapi
-
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def home():
+    return {"Hello": "World"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app")
